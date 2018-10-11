5b4f4dbde1497e41d4b857d6919d271d7554d1ac
Drew Miley <drewmiley92@gmail.com>
2018-10-11 11:12:40 +0100

    Adds fetch to actions

--- app/ducks/actions.ts
+++ app/ducks/actions.ts
@@ -1,6 +1,25 @@
 import * as actiontypes from './actiontypes';
 
-export function setNumber(number: number) {
+export const mapDispatchToProps = (dispatch: Function) => {
+    return { fetchNumber: () => dispatch(fetchNumberFromServer()) };
+};
+
+function fetchNumberFromServer() {
+    return (dispatch: Function) => {
+        fetch('http://localhost:8000/api')
+            .then(response => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response;
+            })
+            .then(response => response.json())
+            .then(number => dispatch(setNumber(number)))
+            .catch(() => console.log('Server failed!!!'));
+    };
+}
+
+function setNumber(number: number) {
     return {
         type: actiontypes.SET_NUMBER,
         number
